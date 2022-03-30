f90009438e7fd9eae1e860ec0f50bef32bff77cb
Issam Mejdoubi (is_sam) <mejdoubi.issam@gmail.com>
2022-03-30 11:33:40 +0200

    Install Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\MarketingSource;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Marketing sources
+        $marketingSources = [
+            'facebook-ads'  => 'Facebook Ads',
+            'google-ads'    => 'Google Ads'
+        ];
+
+        foreach ($marketingSources as $slug => $label) {
+            $marketingSource = new MarketingSource();
+            $marketingSource->setSlug($slug)
+                ->setName($label);
+
+            $manager->persist($marketingSource);
+        }
+        $manager->flush();
+    }
+}
