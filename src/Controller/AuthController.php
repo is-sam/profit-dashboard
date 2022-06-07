279561ab7457ae2f1366c5f4d8b54eae8490f638
Issam <mejdoubi.issam@gmail.com>
2022-06-07 16:19:18 +0200

    Refacto base template & login

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -31,8 +31,8 @@ class AuthController extends AbstractController
             return $this->redirectToRoute('auth_login', ['shop' => $formData['shop']]);
         }
 
-        return $this->render('auth/login.html.twig', [
-            'loginForm' => $loginForm->createView(),
+        return $this->renderForm('auth/login.html.twig', [
+            'loginForm' => $loginForm,
         ]);
     }
 
